b65d5ee85bd89e068a29354ddcb51fac4ec43154
Ignacio Della Morte <ignaciodm93@gmail.com>
2020-11-27 16:32:41 -0300

    first commit

---
+++ Hospitales/Estructuras.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitales
+{
+
+
+    struct Hospital
+    {
+        string nombre;
+        string direccion;
+        List<Medico> medicos;
+        List<Paciente> pacientes;
+        EEspecialidad especialidad;
+
+        public Hospital(string auxNombre, string auxDireccion, EEspecialidad auxEspecialdad)
+        {
+           nombre = auxNombre;
+           direccion = auxDireccion;
+           medicos = new List<Medico>();
+           pacientes = new List<Paciente>();
+           especialidad = auxEspecialdad;
+        }
+
+        /// <summary>
+        /// Metodo para obtener toda la informacion junta del hospital.
+        /// </summary>
+        public void getInformacionHospital()
+        {
+
+
+            Console.WriteLine("----------FICHA DEL HOSPITAL----------");
+            Console.WriteLine("Nombre: {0}", nombre);
+            Console.WriteLine("Dirección: {0}", direccion);
+            Console.WriteLine("Especialidad: {0}", getEspecialidad());
+            Console.WriteLine("--------------------------------------");
+            
+            Console.WriteLine("\n¿Deseas acceder a la informacion de medicos y pacientes?");
+            Console.WriteLine("1. Si \n2. No");
+
+            int numero = 0;
+
+            do
+            {
+                int.TryParse(Console.ReadLine(), out numero);
+
+                if (numero < 1 || numero > 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ingrese un numero valido.");
+                    Console.ResetColor();
+                }
+
+            } while (numero < 1 || numero > 2);
+
+            if (numero == 1)
+            {
+                obtenerMedicos();
+                Console.WriteLine("\nPresione una tecla para continuar..");
+                Console.ReadKey();
+                obtenerPacientes();
+            }
+
+            
+
+
+        }
+
+
+        /// <summary>
+        /// Metodo para obtener la info de todos los medicos, accediendo al metodo getIntoMedico, particular del struct Medico
+        /// </summary>
+        /// <param name="medicos"></param>
+        public void obtenerMedicos()
+        {
+            Console.Clear();
+
+            Console.WriteLine("*****MEDICOS DEL HOSPITAL {0}******\n", nombre.ToUpper());
+
+            foreach (Medico m in medicos)
+            {
+                m.getInfoMedico();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Metodo para obtener pacientes
+        /// </summary>
+        public void obtenerPacientes()
+        {
+            Console.Clear();
+
+            Console.WriteLine("*****PACIENTES DEL HOSPITAL {0}******\n", nombre.ToUpper());
+
+            foreach (Paciente p in pacientes)
+            {
+                p.getInfoPaciente();
+            }
+        }
+
+        /// <summary>
+        /// Metodo para obtener los pacientes de un hospital
+        /// </summary>
+        /// <returns></returns>
+        public List<Paciente> devolverPacientes()
+        {
+            return pacientes;
+        }
+
+        /// <summary>
+        /// Metodo para buscar y devolver un paciente usando su nombre
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <returns></returns>
+        public Paciente buscarPaciente(string nombre)
+        {
+            Paciente pac = new Paciente();
+
+            foreach (Paciente p in pacientes)
+            {
+                if (p.getNombrePaciente() == nombre)
+                {
+                    pac = p;
+                }
+            }
+
+            return pac;
+        }
+
+
+        /// <summary>
+        /// Metodo para agregar medicos
+        /// </summary>
+        public void agregarPaciente(Paciente pacienteNuevo)
+        {
+            pacientes.Add(pacienteNuevo);
+        }
+
+        /// <summary>
+        /// Metodo para sacar el paciente de una lista.
+        /// </summary>
+        /// <param name="p"></param>
+        public void curarPaciente(Paciente p)
+        {
+            pacientes.Remove(p);
+        }
+
+
+        /// <summary>
+        /// Metodo para agergar medico.
+        /// </summary>
+        /// <param name="medicoNuevo"></param>
+        public void AgregarMedico(Medico medicoNuevo)
+        {
+            medicos.Add(medicoNuevo);
+        }
+
+        #region Getters
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public string getEspecialidad()
+        {
+            if ((int)especialidad == 3)
+            {
+                string cg = "Clinica General";
+                return cg;
+            }
+            return especialidad.ToString();
+        }
+
+        public string getDireccion()
+        {
+            return direccion;
+        }
+        #endregion
+
+        
+
+
+
+    }
+
+
+
+
+    enum EEspecialidad
+    {
+        Pediatria = 1, 
+        Quemaduras = 2,
+        ClinicaGeneral = 3
+    }
+
+
+    struct Medico
+    {
+        string nombre;
+        int edad;
+        string direccion;
+        int nLicencia;
+
+        public Medico(string auxNombre, int auxEdad, string auxDireccion, int auxNLicencia)
+        {
+            nombre = auxNombre;
+            edad = auxEdad;
+            direccion = auxDireccion;
+            nLicencia = auxNLicencia;
+        }
+
+        /// <summary>
+        /// Metodo para obtener la informacion del medico
+        /// </summary>
+        public void getInfoMedico()
+        {
+            
+            Console.WriteLine("-----------FICHA DEL MEDICO-----------");
+            Console.WriteLine("Nombre: {0}", nombre);
+            Console.WriteLine("Edad: {0}", edad);
+            Console.WriteLine("Dirección: {0}", direccion);
+            Console.WriteLine("Número de licencia: {0}", nLicencia);
+            Console.WriteLine("--------------------------------------\n");
+        }
+
+
+        #region Getters
+        public string getNombreMedico()
+        {
+            return nombre;
+        }
+
+        public int getNLicencia()
+        {
+            return nLicencia;
+        }
+
+        public string getDireccion()
+        {
+            return direccion;
+        }
+
+        public int getEdad()
+        {
+            return edad;
+        }
+        #endregion
+
+
+
+        #region Setters
+        public void setEdad(int nuevaEdad)
+        {
+            edad = nuevaEdad;
+        }
+
+        public void setDireccion(string nuevaDireccion)
+        {
+            direccion = nuevaDireccion;
+        }
+        #endregion
+
+
+
+    }
+
+
+    struct Paciente
+    {
+        string nombre;
+        int edad;
+        EDolencia dolencia;
+        bool sano;
+        string estado;
+
+        public Paciente(string auxNombre, int auxEdad, EDolencia auxDolencia)
+        {
+            nombre = auxNombre;
+            edad = auxEdad;
+            dolencia = auxDolencia;
+            sano = false;
+            estado = "En tratamiento";
+        }
+
+
+        /// <summary>
+        /// Metodo para obtener los datos actuales del paciente.
+        /// </summary>
+        public void getInfoPaciente()
+        {
+            string estado;
+
+
+
+            Console.WriteLine("----------FICHA DEL PACIENTE----------");
+            Console.WriteLine("Nombre: {0}", nombre);
+            Console.WriteLine("Edad: {0}", edad);
+            Console.WriteLine("Dolencia: {0}", getDolenciaPaciente());
+            Console.WriteLine("Estado: {0}", getEstado());
+            Console.WriteLine("--------------------------------------\n");
+
+        }
+
+
+        #region Getters
+        public string getNombrePaciente()
+        {
+            return nombre;
+        }
+
+        public int getIndicadorDolencia()
+        {
+            return (int)dolencia;
+        }
+
+        public string getDolenciaPaciente()
+        {
+            int indicadorDolencia = getIndicadorDolencia();
+            string dolor = "";
+
+            if (indicadorDolencia == 1 )
+            {
+                dolor = "Dolor de cabeza";
+            }
+            else if (indicadorDolencia == 2)
+            {
+                dolor = "Dolor de espalda";
+            }
+            else if (indicadorDolencia == 3)
+            {
+                dolor = "Quemadura de agua caliente";
+            }
+            else if (indicadorDolencia == 4)
+            {
+                dolor = "Quemadura general";
+            }
+            else if (indicadorDolencia == 5)
+            {
+                dolor = "Gripe";
+            }
+            else
+            {
+                dolor = "sano";
+            }
+
+
+            return dolor;
+            
+        }
+
+        public string getEstado()
+        {
+            return estado;
+        }
+
+        public int getEdad()
+        {
+            return edad;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// Set especial
+        /// </summary>
+        /// <param name="nuevoEstado"></param>
+        public void setEstado(bool nuevoEstado)
+        {
+            //string estado;
+
+            if (nuevoEstado == false)
+            {
+                estado = "En tratamiento";
+            }
+            else
+            {
+                sano = true;
+                estado = "Curado";
+                dolencia = EDolencia.Sano;
+            }
+        }
+
+
+
+    }
+
+    enum EDolencia
+    {
+        DolorDeCabeza = 1,
+        DolorDeEspalda = 2,
+        QuemaduraAguaCaliente = 3,
+        QuemaduraGeneral = 4,
+        Gripezinha = 5,
+        Sano = 6
+    }
+
+
+
+
+}
---
+++ Hospitales/Program.cs
@@ -0,0 +1,550 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitales
+{
+
+    class Program
+    {
+
+        static Hospital Aleman = new Hospital("Aleman", "Roma 753", EEspecialidad.ClinicaGeneral);
+
+        static Hospital Quemado = new Hospital("Del Quemado", "Avenida Light my Fire 1967", EEspecialidad.Quemaduras);
+
+        static Hospital RicardoGutierrez = new Hospital("Dr Ricardo Gutierrez", "Avenida Belgrano 3243", EEspecialidad.Pediatria);
+
+        static List<Paciente> Curados = new List<Paciente>();
+
+        static void Main(string[] args)
+        {
+
+            PreguntarCargaAutomatica();
+
+            Console.Clear();
+
+            Menu();
+
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// Metodo para cargar informacion de base de manera automatica.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="Q"></param>
+        /// <param name="R"></param>
+        public static void PreguntarCargaAutomatica()
+        {
+
+            Console.WriteLine("¿Deseas cargar datos automaticamente?");
+            Console.WriteLine("1. Si \n2. No");
+
+            int ans = validarInt(1, 2);
+
+            if (ans == 1)
+            {
+
+
+                Paciente Mu = new Paciente("Mu", 12, EDolencia.DolorDeCabeza);
+                Paciente Aldebaran = new Paciente("Aldebaran", 17, EDolencia.QuemaduraGeneral);
+                Paciente Saga = new Paciente("Saga", 29, EDolencia.Gripezinha);
+                Paciente Mefisto = new Paciente("Mefisto", 11, EDolencia.DolorDeEspalda);
+                Paciente Aioria = new Paciente("Aioria", 33, EDolencia.DolorDeCabeza);
+                Paciente Shaka = new Paciente("Shaka", 20, EDolencia.QuemaduraAguaCaliente);
+                Paciente Dohko = new Paciente("Dohko", 45, EDolencia.Gripezinha);
+                Paciente Milo = new Paciente("Milo", 30, EDolencia.DolorDeCabeza);
+                Paciente Aioros = new Paciente("Aioros", 60, EDolencia.QuemaduraAguaCaliente);
+
+                DerivarPaciente(Mu);
+                DerivarPaciente(Aldebaran);
+                DerivarPaciente(Saga);
+                DerivarPaciente(Mefisto);
+                DerivarPaciente(Aioria);
+                DerivarPaciente(Shaka);
+                DerivarPaciente(Dohko);
+                DerivarPaciente(Milo);
+                DerivarPaciente(Aioros);
+
+
+                Medico Seiya = new Medico("Seiya", 54, "Avenida Cramer 1976", 1993);
+                Medico Hyoga = new Medico("Hyoga", 36, "Paso del cisne 1770", 4001);
+                Medico Ikki = new Medico("Ikki", 41, "Fenix 66", 2004);
+                Medico Shun = new Medico("Shun", 37, "Pasaje Cadenas 206", 1003);
+
+
+                Aleman.AgregarMedico(Seiya);
+                Aleman.AgregarMedico(Hyoga);
+                Quemado.AgregarMedico(Ikki);
+                RicardoGutierrez.AgregarMedico(Shun);
+
+
+                Console.WriteLine("Datos cargados automaticamente, presione cualquier tecla para continuar..");
+                Console.ReadKey();
+
+            }
+
+
+
+
+        }
+
+
+        /// <summary>
+        /// Metodo para ejecutar el menu.
+        /// </summary>
+        public static void Menu()
+        {
+
+            int res = 0;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("*************SISTEMA DE SALUD*************");
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("¿En que podemos ayudarlo?");
+                Console.WriteLine("1. Ingresar un nuevo paciente.");//tambien lo derivará
+                Console.WriteLine("2. Ver Pacientes.");
+                Console.WriteLine("3. Ver Medicos.");
+                Console.WriteLine("4. Ver Informacion del Hospital.");
+                Console.WriteLine("5. Atender paciente/es.");
+                Console.WriteLine("6. Ver curados");
+                Console.WriteLine("7. Salir del sistema.");
+
+                Console.WriteLine("");
+                CalcularEstadisticas();
+
+                res = validarInt(1, 7);
+               
+                switch (res)
+                {
+                    case 1:
+
+                        IngresarPaciente();
+                        break;
+                    case 2:
+                        Console.Clear();
+
+                        subInformacion("p");
+
+                        int seleccion = validarInt(1, 3);
+
+                        switch (seleccion)
+                        {
+                            case 1:
+                                Aleman.obtenerPacientes();
+                                break;
+                            case 2:
+                                Quemado.obtenerPacientes();
+                                break;
+                            case 3:
+                                RicardoGutierrez.obtenerPacientes();
+                                break;
+                        }
+
+                        Console.WriteLine("\nPresione cualquier tecla para volver al menu.");
+                        Console.ReadKey();
+                        
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+
+                        subInformacion("m");
+
+                        int seleccion2 = validarInt(1, 3);
+
+                        switch (seleccion2)
+                        {
+                            case 1:
+                                Aleman.obtenerMedicos();
+                                break;
+                            case 2:
+                                Quemado.obtenerMedicos();
+                                break;
+                            case 3:
+                                RicardoGutierrez.obtenerMedicos();
+                                break;
+                        }
+
+                        Console.WriteLine("\nPresione cualquier tecla para volver al menu.");
+                        Console.ReadKey();
+
+                        break;
+
+                    case 4:
+                        Console.Clear();
+
+                        subInformacion("h");
+
+                        int seleccion3 = validarInt(1, 3);
+
+                        switch (seleccion3)
+                        {
+                            case 1:
+                                Aleman.getInformacionHospital();
+                                break;
+                            case 2:
+                                Quemado.getInformacionHospital();
+                                break;
+                            case 3:
+                                RicardoGutierrez.getInformacionHospital();
+                                break;
+                        }
+
+
+                        Console.WriteLine("\nPresione cualquier tecla para volver al menu.");
+                        Console.ReadKey();
+
+                        break;
+
+                    case 5:
+
+                        atenderPaciente();
+
+                        break;
+                    case 6:
+
+                        VerCurados();
+
+                        break;
+                    case 7:
+                        Console.WriteLine("\nGracias por usar nuestro sistema de salud!");
+                        Console.WriteLine("Presione cualquier tecla..");
+                        Console.ReadKey();
+                        break;
+                }
+
+
+
+                
+
+
+            } while (res != 7);
+
+
+        }
+
+     
+        /// <summary>
+        /// Metodo para ingresar un nuevo paciente al sistema de salud
+        /// </summary>
+        public static void IngresarPaciente()
+        {
+            Console.Clear();
+
+            Console.WriteLine("¿Nombre del nuevo paciente a ingresar?");
+            string nuevoNombre = ValidarString();
+
+            Console.WriteLine("¿Que edad tiene?");
+            int nuevaEdad = validarInt(0, 110);
+
+            Console.WriteLine("¿Que dolencia tiene?");
+            Console.WriteLine("1. Dolor de cabeza \n2. Dolor de espalda\n3. Quemadura por Agua Caliente\n4. Quemadura General\n5. Gripe");
+            int dolor = validarInt(1, 5);
+
+            EDolencia nuevaDolencia = 0;
+
+            switch (dolor)
+            {
+                case 1:
+                    nuevaDolencia = EDolencia.DolorDeCabeza;
+                    break;
+                case 2:
+                    nuevaDolencia = EDolencia.DolorDeEspalda;
+                    break;
+                case 3:
+                    nuevaDolencia = EDolencia.QuemaduraAguaCaliente;
+                    break;
+                case 4:
+                    nuevaDolencia = EDolencia.QuemaduraGeneral;
+                    break;
+                case 5:
+                    nuevaDolencia = EDolencia.Gripezinha;
+                    break;
+            }
+
+
+            Paciente nuevoPaciente = new Paciente(nuevoNombre, nuevaEdad, nuevaDolencia);
+
+            DerivarPaciente(nuevoPaciente);
+
+        }
+
+        /// <summary>
+        /// Metodo para derivar a los pacientes automaticamente cuando ingresan al sistema de salud.
+        /// </summary>
+        /// <param name="nuevoPaciente"></param>
+        public static void DerivarPaciente(Paciente nuevoPaciente) 
+        {
+
+            if (nuevoPaciente.getEdad() < 15)
+            {
+                RicardoGutierrez.agregarPaciente(nuevoPaciente);
+            }
+            else
+            {
+                int valorDolencia = nuevoPaciente.getIndicadorDolencia();
+
+                if (valorDolencia == 1 || valorDolencia == 2 || valorDolencia == 5)
+                {
+                    Aleman.agregarPaciente(nuevoPaciente);
+                }
+                else
+                {
+                    Quemado.agregarPaciente(nuevoPaciente);
+                }
+            }
+
+        
+        }
+
+
+        /// <summary>
+        /// Metodo para atender pacientes.
+        /// </summary>
+        public static void atenderPaciente()
+        {
+
+            Hospital hospitalATrabjar= escogerHospital();
+            int cuenta = 1;
+
+            Console.WriteLine("\nResultados encontrados:");
+                foreach (Paciente p in hospitalATrabjar.devolverPacientes())
+                {
+                Console.WriteLine("{0}. {1}", cuenta, p.getNombrePaciente());
+                cuenta++;
+                }
+            Console.WriteLine("{0}. Salir", cuenta);
+
+            Console.WriteLine("\n¿A quien deseas atender?");
+
+            int pacienteEscogido = validarInt(1, cuenta+1);
+
+            if (pacienteEscogido != cuenta+1)
+            {   
+
+                cuenta = 1;
+
+                string nombrePaciente = "";
+
+                foreach (Paciente p in hospitalATrabjar.devolverPacientes())
+                {
+                    if (cuenta == pacienteEscogido)
+                    {
+                        nombrePaciente = p.getNombrePaciente();
+                    }
+
+                    cuenta++;
+                }
+
+
+
+                if (nombrePaciente != "")
+                {
+                    hospitalATrabjar.buscarPaciente(nombrePaciente);
+
+                    Curados.Add(hospitalATrabjar.buscarPaciente(nombrePaciente));               
+
+                    hospitalATrabjar.curarPaciente(hospitalATrabjar.buscarPaciente(nombrePaciente));
+
+                    Console.WriteLine("El paciente {0} se curó con exito", nombrePaciente);
+                }
+                     
+
+                
+            }
+
+
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Metodo para... chan chan.. escoger hospital.
+        /// </summary>
+        /// <returns></returns>
+        public static Hospital escogerHospital()
+        {
+            Console.WriteLine("¿Sobre que hospital deseas trabajar?");
+            Console.WriteLine("1. Aleman");
+            Console.WriteLine("2. Quemados");
+            Console.WriteLine("3. Ricardo Gutierrez");
+
+            int ans = validarInt(1, 3);
+
+            if (ans == 1)
+            {
+                return Aleman;
+            }
+            else if (ans == 2)
+            {
+                return Quemado;
+            }
+            else
+            {
+                return RicardoGutierrez;
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// Metodo para mostrar las opciones disponibles.
+        /// </summary>
+        /// <param name="i"></param>
+        public static void subInformacion(string i)
+        {
+
+
+            if (i == "p")
+            {
+                Console.WriteLine("¿De que Hospital quieres ver los pacientes?");
+            }
+            else if (i == "m")
+            {
+                Console.WriteLine("¿De que Hospital quieres ver los medicos?");
+            }
+            else
+            {
+                Console.WriteLine("¿De que hospital quieres ver la informacion?");
+            }
+
+
+            Console.WriteLine("1. Hospital Aleman.\n2. Hospital del Quemado\n3. Hospital Ricardo Gutierrez."); 
+
+        }
+
+
+
+        /// <summary>
+        /// Metodo para validar enteros, tomando los extremos como parametros.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int validarInt(int a, int b)
+        {
+
+            //Console.WriteLine("Ingrese un numero entre {0} y {1}", a , b);
+
+            int numero = 0;
+
+            do
+            {
+
+                int.TryParse(Console.ReadLine(), out numero);
+
+                if (numero < a || numero > b)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ingrese un numero valido.");
+                    Console.ResetColor();
+                }
+
+            } while (numero < a || numero > b);
+
+            return numero;
+        }
+
+        
+        /// <summary>
+        /// Metodo para recorrer los curados
+        /// </summary>
+        public static void VerCurados()
+        {
+            foreach (Paciente curado in Curados)
+            {
+
+                if (curado.getEstado() == "En tratamiento")
+                {
+                    curado.setEstado(true);
+                }
+
+
+                curado.getInfoPaciente();
+            }
+
+            if (Curados.Count() < 1)
+            {
+                Console.WriteLine("\nNo hay pacientes curados :( murieros todos.");
+            }
+
+            Console.WriteLine("Presione una tecla para continuar..");
+            Console.ReadKey();
+
+        }
+
+        
+        /// <summary>
+        /// Metodo para validar string
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <returns></returns>
+        public static string ValidarString()
+        {
+    
+            int basura;
+            string texto;
+
+            do
+            {
+                texto = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(texto) || (int.TryParse(texto, out basura)) == true || string.IsNullOrWhiteSpace(texto))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("El texto esta vacio o es nulo, intente nuevamente.");
+                    Console.ResetColor();
+                }
+                
+
+            } while (string.IsNullOrEmpty(texto) || (int.TryParse(texto, out basura)) == true || string.IsNullOrWhiteSpace(texto));
+
+
+            return texto;
+        }
+        
+        
+        /// <summary>
+        /// Metodo para mostrar la cantidad de recuperados e internados, en el Menu.
+        /// </summary>
+        public static void CalcularEstadisticas()
+        {
+            int curados = 0;
+            int internados = 0;
+
+            curados = Curados.Count();
+
+            int aleman = Aleman.devolverPacientes().Count();
+            int quemado = Quemado.devolverPacientes().Count();
+            int RGutierrez = RicardoGutierrez.devolverPacientes().Count();
+
+            internados = aleman + quemado + RGutierrez;
+
+            //internados =+ RicardoGutierrez.devolverPacientes().Count();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Total de pacientes internados: {0}", internados);
+            Console.ResetColor();
+
+            Console.WriteLine("Aleman: {0}", aleman);
+            Console.WriteLine("Quemados: {0}", quemado);
+            Console.WriteLine("Ricardo Gutierrez: {0}", RGutierrez);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Total de pacientes curados: {0}", curados);
+
+            Console.ResetColor();
+        }
+
+
+    }
+}
